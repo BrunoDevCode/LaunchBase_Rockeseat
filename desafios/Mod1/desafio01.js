6982402bd0eb65832cc11606defb749490afef26
Bruno Henrique dos Santos <brunohenriquesantos272@gmail.com>
2020-06-17 23:29:14 -0300

    Novo desafio concluido

---
+++ desafios/Mod1/desafio01.js
@@ -0,0 +1,57 @@
+/**
+ * Primeiros passos com JS
+ * 
+ * Cálculo de IMC
+ * Crie um programa para calcular o IMC e nível de obesidade de uma pessoa
+ */
+
+const pessoa1 = {
+    nome: 'Carlos',
+    peso: 84,
+    altura: 1.88
+}
+
+imc = pessoa1.peso / (pessoa1.altura * pessoa1.altura)
+
+if (imc >= 30) {
+    console.log(`${pessoa1.nome} você esta acima do peso`);
+} else {
+    console.log(`${pessoa1.nome} você não está acima do peso`);
+}
+
+/**
+ * Cálculo de aposentadoria
+ * 
+ * Crie um programa para calcular a aposentadoria de uma pessoa
+ */
+
+const pessoa2 = {
+    nome: 'Silvana',
+    sexo: 'F',
+    idade: 48,
+    contribuicao: 23
+}
+
+// Teste Homem
+// const pessoa = {
+//     nome: 'Mario',
+//     sexo: "M",
+//     idade: 60,
+//     contribuicao: 35
+// }
+
+timeContribuicao = pessoa.idade + pessoa.contribuicao
+
+if( pessoa2.sexo == "F" ) {
+    if ( timeContribuicao >= 85 ) {
+        console.log(`${pessoa2.nome}, você ja pode se aposentar`)
+    } else {
+        console.log(`${pessoa2.nome}, você ainda não pode se aposentar`)
+    }
+} else {
+    if ( timeContribuicao >= 95) {
+        console.log(`${pessoa2.nome}, você já pode se aposentar`)
+    } else {
+        console.log(`${pessoa2.nome}, você ainda não pode se aponsentar`)
+    }
+}
---
+++ desafios/Mod1/desafio02.js
@@ -0,0 +1,68 @@
+/**
+ * Lidando com objetos e vetores
+ * 
+ * Construção e impressão de dados
+ * 
+ * Crie um programa que armazena dados da empresa Rockeseat dentro de um objeto chamado empresa.
+ */
+
+const empresa = {
+    nome: 'Rocketseat',
+    cor: 'Roxo',
+    foco: 'Programação',
+    endereço: {
+        rua: 'Rua Guilherme Gembala',
+        numero: 260
+    }
+}
+
+console.log(`A empresa ${empresa.nome} está localizada na ${empresa.endereço.rua}, ${empresa.endereço.numero}`)
+
+/**
+ * Vetores e objetos
+ * 
+ * Crie um programa com um objeto para armazenar dados de um progrador como nome, idade e tecnologias que trabalha.
+ * Um programador pode trabalhar com várias tecnologia, por isso armazene essas tecnologias em um array .
+ * As tecnologias também devem ser objetos contendo nome e especialidade.
+ * 
+ * Techs : [
+ *   {nome: "C++", especialidade: "Desktop"},
+ *   {nome: 'Python', especialidade: 'Data Science'},
+ *   {nome: 'Javascript', especialidade: 'Web/Mobile'}
+ * ]
+ */
+
+const dev = {
+    nome: 'Bruno',
+    idade: 18,
+    techs: [
+        { nome: 'Javascript', especialidade: 'Web/Mobile' }
+    ]
+}
+
+console.log(`O Dev ${dev.nome} tem ${dev.idade} anos e usa a tecnologia ${dev.techs[0].nome} com especialidade em ${dev.techs[0].especialidade}`)
+
+// Apenas por curiosidade
+
+const developers = [
+    {
+        nome: 'Bruno',
+        idade: 18,
+        techs: [
+            { nome: "C++", especialidade: "Desktop" }
+        ]
+    },
+    {
+        nome: 'Mayk',
+        idade: 39,
+        techs: [
+            {nome: 'Python', especialidade:'Data Science'}
+        ]
+    }
+]
+
+for (const developer of developers) {
+    console.log(`O Dev ${developer.nome} tem ${developer.idade} anos e usa a tecnologia ${dev.techs[0].nome} com especialidade em ${dev.techs[0].especialidade}`)
+}
+
+// Deu Certo
