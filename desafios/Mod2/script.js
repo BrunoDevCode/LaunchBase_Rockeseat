0fd2d4e2c9a4496aa260e70965eaf582b3a9908b
Bruno Henrique dos Santos <brunohenriquesantos272@gmail.com>
2020-06-24 23:19:26 -0300

    Terminando o ultimo desafio do Modulo de FrontEnd 02-3

--- desafios/Mod2/script.js
+++ desafios/Mod2/script.js
@@ -1,5 +1,6 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
+const modal = document.querySelector('.modal');
 
 for (let card of cards) {
     card.addEventListener('click', function() {
@@ -12,4 +13,12 @@ for (let card of cards) {
 document.querySelector('.close-modal').addEventListener('click', function() {
     modalOverlay.classList.remove('active');
     modalOverlay.querySelector('iframe').src = '';
+})
+
+modalOverlay.querySelector('.maximize-modal').addEventListener('click', function() {
+    if (modal.classList.contains('maximize')) {
+        modal.classList.remove('maximize');
+    } else {
+        modal.classList.add('maximize');
+    }
 })
