dff05e506c4a7ceddd99d74ae6236f43e2537589
Bruno Henrique Santos <brunohenriquesantos272@gmail.com>
2020-06-29 00:24:56 -0300

    Avançando nas aulas

---
+++ aulas/Mod3/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: 'GykTLqODQuU',
+        title: 'Formulario animado com JS puro e CSS Animation | Mayk Brito',
+        duration: '57 min',
+        price: 'Free'
+    },
+    {
+        id: 'ghTrp1x_1As',
+        title: 'Formulario animado com JS puro e CSS Animation | Mayk Brito',
+        duration: '57 min',
+        price: 'Free'
+    },
+    {
+        id: 'HN1UjzRSdBk',
+        title: 'Formulario animado com JS puro e CSS Animation | Mayk Brito',
+        duration: '57 min',
+        price: 'Free'
+    },
+    {
+        id: 'rAzHvYnQ8DY',
+        title: 'Formulario animado com JS puro e CSS Animation | Mayk Brito',
+        duration: '57 min',
+        price: 'Free'
+    },
+    {
+        id: '2alg7MQ6_sI',
+        title: 'Formulario animado com JS puro e CSS Animation | Mayk Brito',
+        duration: '57 min',
+        price: 'Free'
+    },
+    {
+        id: 'GSqR2i',
+        title: 'Formulario animado com JS puro e CSS Animation | Mayk Brito',
+        duration: '57 min',
+        price: 'Free'
+    }
+]
--- aulas/Mod3/server.js
+++ aulas/Mod3/server.js
@@ -2,21 +2,35 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require('./data')
 
 server.use(express.static('public'))
 
 server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get('/', function(res, res) {
-    return res.render('about')
+    const about = {
+        avatar_url: 'https://avatars3.githubusercontent.com/u/45718816?s=400&u=421d3cf1dc5ee8b08263a9c2308fb31d0d1a7824&v=4',
+        name: 'Bruno Henrique',
+        role: 'Desenvolvedor Full-Stack JS',
+        description: 'Desenvolvedor FullStack em JS com o maior prazer para o ramo do desenvolvimento',
+        links: [
+            {name: 'Github', url: 'https://github.com/BrunoDevCode/'},
+            {name: 'Twetter', url: 'https://twitter.com/BrunoH272'},
+            {name: 'LinkedIn', url: 'https://linkedin.com/in/BrunoH5'}
+        ]
+    }
+
+    return res.render('about', {about})
 })
 
 server.get('/portfolio', function(res, res) {
-    return res.render('portifolio')
+    return res.render('portifolio', { items: videos })
 })
 
 server.listen(5000, function() {
