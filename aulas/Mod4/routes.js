c9dac6a4ec3fbe8d43014af5175da1e196511d5c
Bruno Henrique Santos <brunohenriquesantos272@gmail.com>
2020-07-05 23:58:32 -0300

    Adicionando novo comando na aulas assim como CSS de animation e trabalhando com formularios

--- aulas/Mod4/routes.js
+++ aulas/Mod4/routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const routes = express.Router()
 
+const instructors = require('./instructors')
+
 routes.get('/', (req, res) => {
     return res.redirect('/instructors')
 })
@@ -8,6 +10,12 @@ routes.get('/', (req, res) => {
 routes.get('/instructors', (req, res) => {
     return res.render('instructors/index')
 })
+
+routes.get('/instructors/create', (req, res) => {
+    return res.render('instructors/create')
+})
+
+routes.post('/instructors', instructors.post)
 
 routes.get('/members', (req, res) => {
     return res.send('members')
