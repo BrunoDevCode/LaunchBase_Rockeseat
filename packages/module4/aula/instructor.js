b61bb73b78e4b58681f8a0712861f694ef3085f2
Bruno Henrique Santos <brunohenriquesantos272@gmail.com>
2020-11-02 14:11:57 -0300

    Adicionando rota de criação dos instructors e fazendo desacoplamento

---
+++ packages/module4/aula/instructor.js
@@ -0,0 +1,12 @@
+// Create
+// Nome que quiser
+exports.post = (req, res) => {
+  const keys = Object.keys(req.body);
+
+  for (key of keys) {
+    if (req.body[key] == '')
+      return res.send('Please fill all fields');
+  }
+
+  return res.send(req.body);
+}
