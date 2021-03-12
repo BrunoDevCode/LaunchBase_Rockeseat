d897e7e434f92f4d45981488c18a31c822aae63c
Bruno Henrique dos Santos <brunohenriquesantos272@gmail.com>
2021-03-12 12:23:48 -0300

    feat: adding filters in instructors and members

--- packages/module5/aula/src/app/models/instructor.js
+++ packages/module5/aula/src/app/models/instructor.js
@@ -113,4 +113,40 @@ module.exports = {
       return callback();
     });
   },
+
+  paginate(params) {
+    const { filter, limit, offset, callback } = params;
+
+    let query = '',
+      filterQuery = '',
+      totalQuery = `(
+        SELECT count(*) FROM instructors
+      ) AS total`;
+
+    if (filter) {
+      filterQuery = `${query}
+        WHERE instructors.name ILIKE '%${filter}%'
+        OR instructors.services ILIKE '%${filter}%'
+      `;
+
+      totalQuery = `(
+        SELECT count(*) FROM instructors
+        ${filterQuery}
+      ) as total`;
+    }
+
+    query = `
+      SELECT instructors.*, ${totalQuery} , count(members) AS total_students
+      FROM instructors
+      LEFT JOIN members ON (instructors.id = members.instructor_id)
+      ${filterQuery}
+      GROUP BY instructors.id LIMIT $1 OFFSET $2
+    `;
+
+    db.query(query, [limit, offset], (err, results) => {
+      if (err) throw `Database error! ${err}`;
+
+      callback(results.rows);
+    });
+  },
 };
