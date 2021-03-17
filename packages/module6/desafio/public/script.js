79be83125a12b40b86a60401c877d5219df1ce57
Bruno Henrique dos Santos <brunohenriquesantos272@gmail.com>
2021-03-17 00:12:46 -0300

    feat: input mask of porcent and CPF

--- packages/module6/desafio/public/script.js
+++ packages/module6/desafio/public/script.js
@@ -1,25 +1,65 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const cards = document.querySelectorAll('.card');
-const modal = document.querySelector('.modal');
+const Mask = {
+  apply(input, func) {
+    setTimeout(() => {
+      input.value = Mask[func](input.value);
+    }, 1);
+  },
 
-for (let card of cards) {
-    card.addEventListener('click', function() {
-        const iframeId = card.getAttribute('id');
-        window.location.href = `/courses/${iframeId}`
-    })
-}
+  formatPorcent(value) {
+    value = value.replace(/\D/g, '');
+    console.log(value);
+    return (value = new Intl.NumberFormat('pt-BR', {
+      style: 'percent',
+      minimumFractionDigits: 2,
+    }).format(value / 10000));
+  },
 
-document.querySelector('.close-modal').addEventListener('click', function() {
+  formatCPF(value) {
+    value = value.replace(/\D/g, '');
+
+    /**
+     * Primeira linha monta os dados que podem conter na mask
+     * Segunda linha monta o estilo da mask
+     */
+
+    return (value = value.replace(
+      /(\d{3})(\d{3})(\d{3})(\d{2})/,
+      '$1.$2.$3-$4'
+    ));
+  },
+};
+
+function playModal(modalOverlay) {
+  const cards = document.querySelectorAll('.card');
+  const modal = document.querySelector('.modal');
+
+  for (let card of cards) {
+    card.addEventListener('click', function () {
+      const iframeId = card.getAttribute('id');
+      window.location.href = `/courses/${iframeId}`;
+    });
+  }
+
+  document.querySelector('.close-modal').addEventListener('click', function () {
     modalOverlay.classList.remove('active');
     modalOverlay.querySelector('iframe').src = '';
     if (modal.classList.contains('maximize'))
-        modal.classList.remove('maximize')
-})
+      modal.classList.remove('maximize');
+  });
 
-modalOverlay.querySelector('.maximize-modal').addEventListener('click', function() {
-    if (modal.classList.contains('maximize')) {
+  modalOverlay
+    .querySelector('.maximize-modal')
+    .addEventListener('click', function () {
+      if (modal.classList.contains('maximize')) {
         modal.classList.remove('maximize');
-    } else {
+      } else {
         modal.classList.add('maximize');
-    }
-})
+      }
+    });
+}
+
+const modalOverlay = document.querySelector('.modal-overlay');
+
+if (modalOverlay) {
+  playModal(modalOverlay);
+}
