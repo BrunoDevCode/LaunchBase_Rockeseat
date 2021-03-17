cd1234ae628e234a39b250bb548d97d4f9ff8b1b
Bruno Henrique dos Santos <brunohenriquesantos272@gmail.com>
2021-03-17 00:14:45 -0300

    style: prettier

--- packages/module6/desafio/server.js
+++ packages/module6/desafio/server.js
@@ -1,48 +1,47 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
+const express = require('express');
+const nunjucks = require('nunjucks');
 
-const server = express()
-const empresa = require('./data')
+const server = express();
+const empresa = require('./data');
 
-server.use(express.static('public'))
+server.use(express.static('public'));
 
-server.set('view engine', 'njk')
+server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
-    express: server,
-    autoescape: false,
-    noCache: true,
+  express: server,
+  autoescape: false,
+  noCache: true,
 });
 
 server.get('/', (req, res) => {
-    return res.render('links', {empresa})
-})
+  return res.render('links', { empresa });
+});
 
 server.get('/about', (req, res) => {
-    return res.render('about', {empresa})
-})
+  return res.render('about', { empresa });
+});
 
 server.get('/conteudo', (req, res) => {
-    return res.render('conteudo', {empresa})
-})
+  return res.render('conteudo', { empresa });
+});
 
 server.get('/courses/:id', (req, res) => {
-    const id = req.params.id
+  const id = req.params.id;
 
-    const video = empresa.cards.find((cards) => {
-        return cards.id == id
-    })
+  const video = empresa.cards.find((cards) => {
+    return cards.id == id;
+  });
 
-    if(!video)
-        return res.send('Course not found')
+  if (!video) return res.send('Course not found');
 
-    return res.render('courses', {video})
-})
+  return res.render('courses', { video });
+});
 
 server.use((req, res) => {
-    return res.status(404).render('not-found')
-})
+  return res.status(404).render('not-found');
+});
 
 server.listen(3333, () => {
-    console.log('> Server runing')
-})
+  console.log('> Server runing');
+});
